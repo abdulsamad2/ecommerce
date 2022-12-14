b913f967a9403856990060bfa24fd282d9557340
Abdul Samad <abdulsamad.laghari1@gmail.com>
2022-12-14 22:31:08 +0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../images/logo.png";
 const Footer = () => {
   return (
     <div className='bg-stone-100 py-14'>
-      <footer className='max-w-[1140px]  px-4 items-center flex justify-between mx-auto'>
+      <footer className='max-w-[1140px] space-y-14 md:space-y-0 px-4 items-center flex-col md:flex-row flex justify-between mx-auto'>
         {/* Fist Col */}
         <div className='space-y-4'>
           <img
@@ -13,7 +13,7 @@ const Footer = () => {
             alt=''
           />
 
-          <p className='max-w-xs'>
+          <p className='md:max-w-xs'>
             <span className='font-bold uppercase mr-2 '>Address:</span>
             7532 High Point Dr. Lawrenceville, GA 30043
           </p>
@@ -27,26 +27,28 @@ const Footer = () => {
           </p>
         </div>
         {/* Col-2 */}
-        <div className='space-y-3  '>
-          <h2 className='font-bold'>MENU</h2>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>FAQ</li>
-          </ul>
-        </div>
-        <div className='space-y-3 '>
-          <h2 className='font-bold'>ACCOUNT</h2>
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>FAQ</li>
-          </ul>
+        <div className='flex w-full md:w-auto space-x-20'>
+          <div className='space-y-3  '>
+            <h2 className='font-bold'>MENU</h2>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
+          <div className='space-y-3 '>
+            <h2 className='font-bold'>ACCOUNT</h2>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>FAQ</li>
+            </ul>
+          </div>
         </div>
 
-        <div className='space-y-3 ml-4 pl-24 '>
+        <div className='space-y-3 ml-4 w-full md:w-auto md:pl-24 '>
           <h2 className='font-bold'>
             <div className='space-y-3 '>
               <h2 className='font-bold'>NEWSLETTER</h2>
@@ -57,11 +59,11 @@ const Footer = () => {
           </p>
           <div className='border-2 border-gray-200'>
             <input
-              className='py-3 px-2  '
+              className='py-3 w-full md:w-auto px-2  '
               type='email'
               placeholder='Enter You Mail'
             />
-            <button className='bg-red-600 py-3 px-4 text-white'>
+            <button className='bg-red-600 mt-4 md:mt-0 py-3 px-4 text-white'>
               Subscribe
             </button>
           </div>
